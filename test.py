648c52e45dae26094b55f2d81d063d8f38ae4b09
Jang-DongHo <donghoo18@naver.com>
2023-02-21 13:16:16 +0900

    [Feat] 테스트

--- test.py
+++ test.py
@@ -1,13 +1,16 @@
 from PublicDataReader.config.database import conn
 import pymysql
+import pandas as pd
 
 # Connection 으로부터 Cursor 생성
 curs = conn.cursor(pymysql.cursors.DictCursor)
 
 # SQL문 실행
-sql = "select * from 단독다가구매매"
+sql = "select * from 단독다가구전월세 where 월=7"
 curs.execute(sql)
  
 # 데이타 Fetch
 rows = curs.fetchall()
-print(rows)     # 전체 rows
+delivery = pd.DataFrame(rows)
+
+print(delivery.info())
